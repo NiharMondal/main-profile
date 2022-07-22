3f2f40ee308f1d6fa400f840e6334f5659076247
Nihar Mondal <niharmondol9@gmail.com>
2022-07-22 14:12:26 +0600

    project scroll fixed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,40 +7,30 @@ import { AiFillGithub, AiOutlineDribbble } from "react-icons/ai";
 import Tooltip from "react-simple-tooltip";
 function ProjectList({ project }) {
   return (
-    <div className={styles.project_details}>
-      <div className={styles.img_holder}>
-        <div
-          className={styles.project_img}
-          style={{
-            background: `url("${project.img}")`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className={styles.project_info}>
-            <h4>{project.title}</h4>
-            <p>{project.description}</p>
-            <div className={styles.technology}>
-              {project.technology.map((tech, index) => {
-                return (
-                  <p className={styles.tech_fix} key={index}>
-                    {tech}
-                  </p>
-                );
-              })}
-            </div>
-            <div className={styles.project_link}>
-              <a href={project.liveLink} target="blank">
-                <Tooltip content="Website">
-                  <AiOutlineDribbble />
-                </Tooltip>
-              </a>
-              <a href={project.clientRepo} target="blank">
-                <Tooltip content="GitHub">
-                  <AiFillGithub />
-                </Tooltip>
-              </a>
-            </div>
+    <div className={styles.portfolio_projects}>
+      <div className={styles.img_wrapper}>
+        <img src={project.img} alt="" className={styles.project_img} />
+        <div className={styles.project_info}>
+          <h4>{project.title}</h4>
+          <p>{project.description}</p>
+          <div className={styles.technology_wrapper}>
+            {project.technology.map((tech, index) => (
+              <p key={index} className={styles.technology}>
+                {tech}
+              </p>
+            ))}
+          </div>
+          <div className={styles.project_link}>
+            <a href={project.liveLink} target="blank">
+              <Tooltip content="Website">
+                <AiOutlineDribbble />
+              </Tooltip>
+            </a>
+            <a href={project.clientRepo} target="blank">
+              <Tooltip content="GitHub">
+                <AiFillGithub />
+              </Tooltip>
+            </a>
           </div>
         </div>
       </div>
@@ -66,14 +56,15 @@ const Projects = () => {
     },
     tablet: {
       breakpoint: { max: 1024, min: 768 },
-      items: 1,
+      items: 2,
       slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 767, min: 0 },
+      breakpoint: { max: 768, min:0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
+    
   };
   return (
     <div id="projects">
@@ -83,20 +74,20 @@ const Projects = () => {
             My Projects <span>....</span>
           </h2>
         </div>
-
-        <Carousel
-          responsive={responsive}
-          swipeable={true}
-          draggable={true}
-          showDots={true}
-          infinite={true}
-          keyBoardControl={true}
-          removeArrowOnDeviceType={["tablet", "mobile"]}
-        >
-          {myProjects.map((project) => {
-            return <ProjectList project={project} key={project.title} />;
-          })}
-        </Carousel>
+        <div>
+          <Carousel
+            responsive={responsive}
+            swipeable={true}
+            draggable={true}
+            infinite={true}
+            keyBoardControl={true}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+          >
+            {myProjects.map((project, index) => {
+              return <ProjectList project={project} key={index} />;
+            })}
+          </Carousel>
+        </div>
       </div>
     </div>
   );
